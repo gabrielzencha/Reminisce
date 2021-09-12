303c0a022b9104403ff2f549139b3304fd1116d3
gabrielzencha <gzencha.ashungafac@tedu.edu.tr>
2021-09-12 17:06:17 +0300

    user guide completed

--- Components/AppComponents/UserGuide.js
+++ Components/AppComponents/UserGuide.js
@@ -1,54 +1,57 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, FlatList } from 'react-native'
+import React, { Component } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 export default class UserGuide extends Component {
-    render() {
-        return (
-                <View style={styles.container}>
-                    <Text style={{
-                        fontSize: 30,
-                        fontWeight: 'bold',
-                        padding: 30,
-                    }}>
-                        How To Use Reminisce
-                    </Text>
-                    <FlatList data ={[
-                        {
-                            id: 1,
-                            text: "Tab the pencil in the About Me page and give some information about yourself."
-                        }, 
-                        {
-                            id: 2,
-                            text: "To send a new text, go to Messages, tab the pencil, type and send the text."
-                        }, 
-                        {
-                            id: 3,
-                            text: "To respond to someone’s message, click on that person’s message, type your reply, and click send."
-                        }, 
-                        {
-                            id: 4,
-                            text: "You will get notifications when someone living locally sends a new message or replies to your message."
-                        }, 
-                        {
-                            id: 5,
-                            text: "Surprise!!! Tab the Memories tab and travel down the memory lane"
-                        }, 
-                       
-                    ]}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            padding: 30,
+            alignItems: "center",
+          }}
+        >
+          How To Use Reminisce
+        </Text>
 
-                   
+        <Text style={styles.textBody}>
+          1) Tab the pencil in the About Me page and give some information about
+          yourself.
+        </Text>
 
-                    />
-                </View>
-           
-        )
-    }
+        <Text style={styles.textBody}>
+          2) To send a new text, go to Messages, tab the pencil, type and send
+          the text.
+        </Text>
+
+        <Text style={styles.textBody}>
+          3) To respond to someone’s message, click on that person’s message,
+          type your reply, and click send.
+        </Text>
+
+        <Text style={styles.textBody}>
+          4) You will get notifications when someone living locally sends a new
+          message or replies to your message.
+        </Text>
+
+        <Text style={styles.textBody}>
+          5) Surprise!!! Tab the Memories tab and travel down the memory lane
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+  },
+  textBody: {
+      padding: 5,
+    fontSize: 22,
+    fontWeight: '500',
+  },
+});
