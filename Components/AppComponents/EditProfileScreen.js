e1c78be98e10520cd05762eca16fbdfd7bb4dc11
gabrielzencha <gzencha.ashungafac@tedu.edu.tr>
2021-09-12 15:56:03 +0300

    edit profile screen updated

--- Components/AppComponents/EditProfileScreen.js
+++ Components/AppComponents/EditProfileScreen.js
@@ -1,10 +1,283 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ImageBackground, TextInput, StyleSheet, Platform } from 'react-native';
+import { FontAwesome, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function EditProfileScreen() {
     return (
-        <View>
-            <Text>Edit Profile Screen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={{margin: 20}}>
+                <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <View style={{
+                            height: 100,
+                            width: 100,
+                            borderRadius: 15,
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                            <ImageBackground
+                             source={require("../../assets/images/logo.png")}
+                             style={{
+                                 height: 100,
+                                 width: 100,
+                             }}
+                             imageStyle={{borderRadius: 15}}
+                            
+                            >
+                                <View style={{
+                                    flex: 1,
+                                    justifyContent: 'center',
+                                    alignItems:'center'
+                                }}>
+                                    <MaterialCommunityIcons name="camera" color="#fff" size={35} style={{
+                                        opacity: 0.7,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderWidth: 1,
+                                        borderColor: "#fff",
+                                        borderRadius: 10
+                                    }} />
+                                </View>
+                            </ImageBackground>
+
+                        </View>
+
+                    </TouchableOpacity>
+                    <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
+                        John Doe
+                    </Text>
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='user-o' size={20}/>
+                    <TextInput
+                    placeholder="First Name and Last Name"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+              
+                <View style={styles.action}>
+                    <FontAwesome name='phone' size={20}/>
+                    <TextInput
+                    placeholder="Telephone Number"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="thumbs-up" size={20}/>
+                    <TextInput
+                    placeholder="My Likes"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='thumbs-down' size={20}/>
+                    <TextInput
+                    placeholder="My Dislikes"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="history" size={20}/>
+                    <TextInput
+                    placeholder="Story of my life"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="address-book" size={20}/>
+                    <TextInput
+                    placeholder="First Emergency Contact Name"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='link' size={20}/>
+                    <TextInput
+                    placeholder="Relationship"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="envelope" size={20}/>
+                    <TextInput
+                    placeholder="First Emergency Contact Email"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="phone" size={20}/>
+                    <TextInput
+                    placeholder="First Emergency Contact Telephone"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="address-book" size={20}/>
+                    <TextInput
+                    placeholder="Second Emergency Contact Name"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='link' size={20}/>
+                    <TextInput
+                    placeholder="Relationship"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="envelope" size={20}/>
+                    <TextInput
+                    placeholder="Second Emergency Contact Email"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="phone" size={20}/>
+                    <TextInput
+                    placeholder="Second Emergency Contact Telephone"
+                    placeholderTextColor = "#666666"
+                    style ={styles.textInput}
+                    autoCorrect ={false}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name="question" size={20}/>
+                    <TextInput
+                    placeholder="Will you be using Reminisce yourself?"
+                    placeholderTextColor = "#666666"
+                    style ={[styles.textInput], {
+                        flexWrap: 'wrap',
+                        fontSize: 20,
+                        paddingLeft: 10,
+                    }}
+                    autoCorrect ={false}
+                    />
+                </View>
+
+                <View style={[styles.action], {
+                    flexDirection: 'column'
+                }}>
+                    <View style={{
+                        flexDirection: 'row'
+                    }}>
+                    <FontAwesome name="question" size={20}/>
+                    <Text style={ {color: "#00000f", fontSize: 20, paddingLeft: 10}}>Which of your emegerncy contacts will use Reminsce on your behalf?
+                     </Text>
+                    </View>
+                     <TextInput style={styles.textInput} placeholder="type your response here"/>
+                </View>
+            </View>
+            <TouchableOpacity onPress= {() => {}} style={styles.commandButton}>
+                    <Text style={styles.panelButtonTitle}>Submit</Text>
+            </TouchableOpacity>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    commandButton: {
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: "#ff5b5b",
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    panel: {
+        padding: 20,
+        backgroundColor: "#ffffff",
+        paddingTop: 20,
+    },
+    header: {
+        backgroundColor: "#fff",
+        shadowColor: "#333333",
+        shadowOffset: {width: -1, height: -3},
+        shadowRadius: 2,
+        shadowOpacity: 0.4,
+        paddingTop: 20,
+        borderTopLeftRadius:20,
+        borderTopRightRadius: 20,
+    },
+    panelHeader: {
+        alignItems: 'center'
+    },
+    pandelHandle: {
+        width: 40, 
+        height: 8, 
+        borderRadius: 4,
+        backgroundColor: "#000040",
+        marginBottom: 10,
+    },
+    pandelTitle: {
+        fontSize: 27,
+        height: 35,
+    },
+    panelSubtitle: {
+        fontSize: 14,
+        color: 'gray',
+        height: 30,
+        marginBottom: 10,
+    },
+    panelButton: {
+        padding: 13,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        marginVertical: 7,
+    },
+    panelButtonTitle: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#00000f',
+        paddingBottom: 5,
+    },
+    actionError: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ff0000',
+        paddingBottom: 5,
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS ==='ios'? 0: -12,
+        paddingLeft: 10,
+        color: '#00000f',
+        fontSize: 20,
+    }
+})
--- Components/AppComponents/UserGuide.js
+++ Components/AppComponents/UserGuide.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, FlatList } from 'react-native'
 
 export default class UserGuide extends Component {
     render() {
         return (
-            <View>
-                <Text> User Guid </Text>
-            </View>
+                <View style={styles.container}>
+                    <Text style={{
+                        fontSize: 30,
+                        fontWeight: 'bold',
+                        padding: 30,
+                    }}>
+                        How To Use Reminisce
+                    </Text>
+                    <FlatList data ={[
+                        {
+                            id: 1,
+                            text: "Tab the pencil in the About Me page and give some information about yourself."
+                        }, 
+                        {
+                            id: 2,
+                            text: "To send a new text, go to Messages, tab the pencil, type and send the text."
+                        }, 
+                        {
+                            id: 3,
+                            text: "To respond to someone’s message, click on that person’s message, type your reply, and click send."
+                        }, 
+                        {
+                            id: 4,
+                            text: "You will get notifications when someone living locally sends a new message or replies to your message."
+                        }, 
+                        {
+                            id: 5,
+                            text: "Surprise!!! Tab the Memories tab and travel down the memory lane"
+                        }, 
+                       
+                    ]}
+
+                   
+
+                    />
+                </View>
+           
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+})
