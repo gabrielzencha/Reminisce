ef7a44e37cba37bc5e772978166b3142ea8303b2
Sunita Mabu <nsunitamabu@yahoo.com>
2021-10-03 13:48:44 +0100

    memories questions included

--- Components/AppComponents/memdata.js
+++ Components/AppComponents/memdata.js
@@ -7,38 +7,38 @@ export default [
     },
     {
         id: '2',
-        questions: '',
-        title: 'Bush Radio',
+        questions: 'What sort of programmes were on the radio?\nWhy did you like your favourite programme?\n',
+        title: 'Bush radio',
         image: require('../../assets/images/local_walks.jpg')
     },
     {
         id: '3',
-        questions: '',
+        questions: 'What was your first Butlins Holiday Camp experience like?\nWhat sort of activities did you carry out at the camp?\n',
         title: 'Butlins Holiday Camp',
         image: require('../../assets/images/face_to_face.jpg')
     },
     {
         id: '4',
-        questions: '',
+        questions: 'Who was Cary Grant?\nWho did you go to cinemas with?\n',
         title: 'Cary Grant',
         image: require('../../assets/images/digital_inclusion.jpg')
     },
     {
         id: '5',
-        questions: '',
-        title:'Christmas Tree',
+        questions: 'What is the Christmas decoration on the tree called?\nWhat other decorations did you put on Christmas trees?\n',
+        title:'Christmas tree',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '6',
-        questions: '',
-        title:'Clothes Line',
+        questions: 'How did you do laundry?\nHow did you dry clothes in Winter?\n',
+        title:'Clothes line',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '7',
-        questions: '',
-        title:'Coffee Maker',
+        questions: 'What coffee brands do you remember?\nHow different is coffee now?\n',
+        title:'Coffee maker',
         image: require('../../assets/images/logo.png')
     },
     {
@@ -49,109 +49,109 @@ export default [
     },
     {
         id: '9',
-        questions: '',
+        questions: 'How did you shop for corsets?\nHow often did you wear corsets?\n',
         title:'Corset',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '10',
-        questions: '',
+        questions: 'What are your memories of Elizabeth Taylor?\nCan you name some of her movies?\n',
         title:'Elizabeth Taylor',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '11',
-        questions: '',
+        questions: 'Which of his songs were used at weddings?\nWhich other rock and roll artists do you remember?\n',
         title:'Elvis Presley',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '12',
-        questions: '',
-        title:'Family Life',
+        questions: 'What do you miss the most about living together as a family?\nWhat was a normal Saturday like with the family?\n ',
+        title:'Family life',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '13',
-        questions: '',
-        title:'First Birth',
+        questions: 'Can you reminisce your first birth experience?\nWhat was the feeling after birth?\n',
+        title:'First birth',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '14',
-        questions: '',
+        questions: 'What memories come to mind when you see a hat?\nWhat special person did you meet at a social event?\n',
         title:'Hats',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '15',
-        questions: '',
-        title:'Marilyn Monroe',
+        questions: 'What do you remember about her?\nWhat in your opinion was she most famous for?\n',
+        title:'Who was she?',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '16',
-        questions: '',
+        questions: 'What car did you own?\nWhat other ways of transportation did you use?\n',
         title:'Morris Manor',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '17',
-        questions: '',
-        title:'Phoenix Metro Cooker',
+        questions: 'What recipes did you make?\nHow different is cooking and eating when compared to the past?\n',
+        title:'Phoenix Metro cooker',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '18',
-        questions: '',
-        title:'Queens Coronation',
+        questions: 'When did the Queen\'s coronation take place?\nWhat are your memories of that day?\n',
+        title:'The Queen\'s Coronation',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '19',
-        questions: '',
-        title:'Singer Sewing Machine',
+        questions: 'What was the most memorable thing your ever sewed?And why?\nDid you knit or crochet as well?\n',
+        title:'Singer sewing machine',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '20',
-        questions: '',
-        title:'Sunday Night at the Palladuim',
+        questions: 'Do you remember the names of the 4 journalists in this show?\nWhat did you enjoy the most about this show?\n',
+        title:'Sunday Night at the Palladium',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '21',
-        questions: '',
+        questions: 'What are your memories of the swinging 60\'s?\nWhat changes came with the swinging 60\'s?\n',
         title:'Swinging 60\'s',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '22',
-        questions: '',
+        questions: 'Which of The Beatles show did you attend?\nHow was the most remarkable show you ever attended?\n ',
         title:'The Beatles',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '23',
-        questions: '',
-        title:'The way to a man\'s heart cookbok',
+        questions: 'Do you think the way to a man\'s heart is through his belly?\nWhat other cookbooks did you use?\n',
+        title:'The way to a man\'s heart cookbook',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '24',
-        questions: '',
-        title:'Weding day',
+        questions: 'What did you wear on your wedding day?\nWhat are your memories of the first dance and the music?\n',
+        title:'Wedding day',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '25',
-        questions: '',
-        title:'What\s my line?',
+        questions: 'What sort of show was What\'s My Line?\nWhich celebrity panelist do you remember attending the show?\n',
+        title:'What\'s My Line?',
         image: require('../../assets/images/logo.png')
     },
     {
         id: '26',
-        questions: '',
+        questions: 'What was The Blue Lamp about?\nWho were the actors and actresses?\n',
         title:'The Blue Lamp',
         image: require('../../assets/images/logo.png')
     },
